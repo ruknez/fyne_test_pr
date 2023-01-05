277294a5a780a3073326638ff22e115e4df9fe31
dima <dima@192.168.1.3>
2023-01-05 10:08:23 +0300

    menu items

---
+++ cmd/menu_item/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Registration")
+	w.Resize(fyne.Size{Width: 600, Height: 600})
+
+	// главное меню -> дочернее меню -> кнопки
+
+	file_item := fyne.NewMenuItem("New file", func() {
+		fmt.Println("Do somting")
+	})
+
+	save_item := fyne.NewMenuItem("save_item", func() {
+		fmt.Println("Do save")
+	})
+
+	// нет смысла fyne сам добавит такую кнопку
+	exit_item := fyne.NewMenuItem("exit_item", func() {
+		os.Exit(1)
+	})
+
+	menu := fyne.NewMenu("File", file_item, save_item, exit_item)
+
+	exitIt := fyne.NewMenuItem("exit item", func() {
+		fmt.Println("edit item")
+	})
+	editMenu := fyne.NewMenu("edit", exitIt)
+
+	mainMenu := fyne.NewMainMenu(menu, editMenu)
+
+	w.SetMainMenu(mainMenu)
+	w.ShowAndRun()
+}
